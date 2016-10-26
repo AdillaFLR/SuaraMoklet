7025c94812dff54951a8248105d626670e39678f
adillaFLR <adillafedelia698@gmail.com>
2016-10-26 13:14:01 -0700

    123

---
+++ app/src/test/java/id/sch/smktelkom_mlg/project/xiirpl301112131/suaramoklet/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package id.sch.smktelkom_mlg.project.xiirpl301112131.suaramoklet;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
