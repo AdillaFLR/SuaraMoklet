3b95d210c45e35d8e62b118161296fd16f00edaa
adillaFLR <adillafedelia698@gmail.com>
2016-11-02 21:07:21 +0700

    menampilkan judul dan deskripsi dari firebase

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl301112131/suaramoklet/AdminPanel.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl301112131/suaramoklet/AdminPanel.java
@@ -1,6 +1,5 @@
 package id.sch.smktelkom_mlg.project.xiirpl301112131.suaramoklet;
 
-import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -10,23 +9,23 @@ import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
-import org.w3c.dom.Text;
-
 public class AdminPanel extends AppCompatActivity {
-    TextView mbarang;
+    TextView tampiljudul,tampildesk;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_panel);
 
-        mbarang = (TextView) findViewById(R.id.coba);
+        tampiljudul = (TextView) findViewById(R.id.judul);
+        tampildesk = (TextView) findViewById(R.id.deskripsi);
+
         Firebase.setAndroidContext(this);
         Firebase ref = new Firebase("https://suaramoklet.firebaseio.com/aspirasi/kesiswaan/ak01/judul");
-
+        Firebase ref2 = new Firebase("https://suaramoklet.firebaseio.com/aspirasi/kesiswaan/ak01/deskripsi");
                     ref.addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
-                    mbarang.setText(dataSnapshot.getValue().toString());
+                    tampiljudul.setText(dataSnapshot.getValue().toString());
                 }
 
             @Override
@@ -34,5 +33,16 @@ public class AdminPanel extends AppCompatActivity {
 
             }
         });
+       ref2.addValueEventListener(new ValueEventListener() {
+           @Override
+           public void onDataChange(DataSnapshot ds) {
+            tampildesk.setText(ds.getValue().toString());
+           }
+
+           @Override
+           public void onCancelled(FirebaseError firebaseError) {
+
+           }
+       });
     }
 }
