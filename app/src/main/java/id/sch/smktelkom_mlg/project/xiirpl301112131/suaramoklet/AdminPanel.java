11b6ac6041b7db95d971e07119f3f2a06f9054ed
Nabila Qonitah Lishandary <nabilaql98@gmail.com>
2016-11-20 15:19:10 +0700

    - Recycler View Firebase

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl301112131/suaramoklet/AdminPanel.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl301112131/suaramoklet/AdminPanel.java
@@ -1,47 +1,182 @@
 package id.sch.smktelkom_mlg.project.xiirpl301112131.suaramoklet;
-
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-import com.firebase.client.DataSnapshot;
+
 import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
-import com.firebase.client.ValueEventListener;
+
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+
+import id.sch.smktelkom_mlg.project.xiirpl301112131.suaramoklet.adapter.AdminAdapter;
 
 public class AdminPanel extends AppCompatActivity {
-    TextView tampiljudul,tampildesk;
+
+    private final static String SAVED_ADAPTER_ITEMS = "SAVED_ADAPTER_ITEMS";
+    private final static String SAVED_ADAPTER_KEYS = "SAVED_ADAPTER_KEYS";
+
+    private com.firebase.client.Query mQuery;
+    private AdminAdapter mAdminAdapter;
+    private ArrayList<Aspirasi> mAdapterItems;
+    private ArrayList<String> mAdapterKeys;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_panel);
 
-        tampiljudul = (TextView) findViewById(R.id.judul);
-        tampildesk = (TextView) findViewById(R.id.deskripsi);
-
-        Firebase.setAndroidContext(this);
-        Firebase ref = new Firebase("https://suaramoklet.firebaseio.com/aspirasi/kesiswaan/ak01/judul");
-        Firebase ref2 = new Firebase("https://suaramoklet.firebaseio.com/aspirasi/kesiswaan/ak01/deskripsi");
-                    ref.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot ds) {
-                    tampiljudul.setText(ds.getValue().toString());
-                }
+        handleInstanceState(savedInstanceState);
+        setupFirebase();
+        setupRecyclerview();
+        Button btn = (Button)findViewById(R.id.buttonRefresh);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCancelled(FirebaseError firebaseError) {
-
+            public void onClick(View v) {
+                mAdminAdapter.notifyDataSetChanged();
             }
         });
-       ref2.addValueEventListener(new ValueEventListener() {
-           @Override
-           public void onDataChange(DataSnapshot ds) {
-            tampildesk.setText(ds.getValue().toString());
-           }
+    }
 
-           @Override
-           public void onCancelled(FirebaseError firebaseError) {
+    // Restoring the item list and the keys of the items: they will be passed to the adapter
+    private void handleInstanceState(Bundle savedInstanceState) {
+        if (savedInstanceState != null &&
+                savedInstanceState.containsKey(SAVED_ADAPTER_ITEMS) &&
+                savedInstanceState.containsKey(SAVED_ADAPTER_KEYS)) {
+            mAdapterItems = Parcels.unwrap(savedInstanceState.getParcelable(SAVED_ADAPTER_ITEMS));
+            mAdapterKeys = savedInstanceState.getStringArrayList(SAVED_ADAPTER_KEYS);
+        } else {
+            mAdapterItems = new ArrayList<Aspirasi>();
+            mAdapterKeys = new ArrayList<String>();
+        }
+    }
 
-           }
-       });
+    private void setupFirebase() {
+        Firebase.setAndroidContext(this);
+        String firebaseLocation = getResources().getString(R.string.firebase_location);
+        Firebase ref = new Firebase("https://suaramoklet.firebaseio.com/aspirasi/admin");
+        mQuery = ref.orderByChild("judul");
+
+    }
+
+     private void setupRecyclerview() {
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerViewAdmin);
+        mAdminAdapter = new AdminAdapter(mQuery, Aspirasi.class, mAdapterItems, mAdapterKeys);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(mAdminAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return item.getItemId() == R.id.action_settings || super.onOptionsItemSelected(item);
+    }
+
+    // Saving the list of items and keys of the items on rotation
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable(SAVED_ADAPTER_ITEMS, Parcels.wrap(mAdminAdapter.getItems()));
+        outState.putStringArrayList(SAVED_ADAPTER_KEYS, mAdminAdapter.getKeys());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mAdminAdapter.destroy();
     }
 }
+
+//import android.support.design.widget.FloatingActionButton;
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//import android.support.v7.widget.LinearLayoutManager;
+//import android.support.v7.widget.RecyclerView;
+//import android.support.v7.widget.Toolbar;
+//import android.view.View;
+//import android.widget.ArrayAdapter;
+//import android.widget.Button;
+//import android.widget.TextView;
+//import com.firebase.client.DataSnapshot;
+//import com.firebase.client.Firebase;
+//import com.firebase.client.FirebaseError;
+//import com.firebase.client.ValueEventListener;
+//import com.firebase.ui.database.FirebaseListAdapter;
+//import com.firebase.ui.database.FirebaseRecyclerAdapter;
+//
+//import java.util.ArrayList;
+//
+//public class AdminPanel extends AppCompatActivity
+//    implements ItemAddedHandler {
+//        Firebase ref = new Firebase("https://suaramoklet.firebaseio.com/aspirasi/admin/id/deskripsi")
+//    //data
+//       ArrayList<String> Items = new ArrayList<>();
+//       ArrayAdapter adapter;
+//    //UI
+//       RecyclerView RecyclerView;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_admin_panel);
+////        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+//
+//        RecyclerView = (RecyclerView) findViewById(R.id.recyclerViewAdmin);
+//        RecyclerView.setHasFixedSize(true);
+//        RecyclerView.setLayoutManager(new LinearLayoutManager(this));
+//
+//        //floatingactionbutton
+////        Button fab = (FloatingActionButton) findViewById(R.id);
+//    }
+////    void onItemAdded(String item) {ref.push().setValue(item); }
+////
+////
+////        ted void onStart() {
+////        per.onStart();
+//
+//        FirebaseRecyclerAdapter<String,MessageViewHolder> adapter =
+//                new FirebaseRecyclerAdapter<String,MessageViewHolder>(
+//                        String.class,
+//                        android.R.layout.two_line_list_item,
+//                        MessageViewHolder.class,
+//                        ref
+//                )
+//                {
+//                    @Override
+//                     protected void populateViewHolder(MessageViewHolder viewHolder, String model, int position) {
+//                    messageViewHolder.mText.setText(s);
+//                    }
+//                };
+//    };
+//
+//            @Override
+//            public void onItemAdded(String item) { ref.push().setValue(item);
+//         }
+//            @Override
+//            protected void onStart() {
+//            super.onStart();
+//        }
+//
+//    public static class MessageViewHolder
+//            extends RecyclerView.ViewHolder {
+//            TextView mText;
+//
+//
+//        public MessageViewHolder(View v){
+//                super(v);
+//            mText =  (TextView) v.findViewById(android.R.id.text1);
+//         }
+//        }
+//    }
---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl301112131/suaramoklet/adapter/AdminAdapter.java
@@ -0,0 +1,79 @@
+package id.sch.smktelkom_mlg.project.xiirpl301112131.suaramoklet.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import com.firebase.client.Query;
+
+import java.util.ArrayList;
+
+import id.sch.smktelkom_mlg.project.xiirpl301112131.suaramoklet.Aspirasi;
+import id.sch.smktelkom_mlg.project.xiirpl301112131.suaramoklet.R;
+
+/**
+ * Created by nabila on 11/20/2016.
+ */
+public class AdminAdapter  extends FirebaseRecyclerAdapter<AdminAdapter.ViewHolder, Aspirasi> {
+
+
+
+    public AdminAdapter(Query mQuery, Class<Aspirasi> itemClass, ArrayList<Aspirasi> mAdapterItems, ArrayList<String> mAdapterKeys) {
+        super(mQuery, itemClass, mAdapterItems, mAdapterKeys);
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+            TextView textViewJudul;
+            TextView textViewDeskripsi;
+
+            public ViewHolder(View view) {
+                super(view);
+                textViewJudul = (TextView) view.findViewById(R.id.textview_judul);
+                textViewDeskripsi = (TextView) view.findViewById(R.id.textview_deskripsi);
+            }
+        }
+
+
+        @Override
+        public AdminAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.aspirasi_item, parent, false);
+
+            return new ViewHolder(view);
+        }
+
+    @Override
+        public void onBindViewHolder(AdminAdapter.ViewHolder holder, int position) {
+            Aspirasi item = getItem(position);
+            holder.textViewJudul.setText(item.getJudul());
+            holder.textViewDeskripsi.setText(item.getDeskripsi());
+        }
+
+    @Override
+    protected void itemAdded(Aspirasi item, String key, int position) {
+
+    }
+
+    @Override
+    protected void itemChanged(Aspirasi oldItem, Aspirasi newItem, String key, int position) {
+
+    }
+
+    @Override
+    protected void itemRemoved(Aspirasi item, String key, int position) {
+
+    }
+
+    @Override
+    protected void itemMoved(Aspirasi item, String key, int oldPosition, int newPosition) {
+
+    }
+
+}
+
+
