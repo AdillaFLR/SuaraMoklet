0271849044961e3ec897dfeceacf749054e21f83
rioranup <rioranu2@gmail.com>
2016-11-21 00:00:56 +0700

    Form login untuk connect ke Firebase

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl301112131/suaramoklet/Login.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl301112131/suaramoklet/Login.java
@@ -1,16 +1,101 @@
 package id.sch.smktelkom_mlg.project.xiirpl301112131.suaramoklet;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * Created by Asus X450 on 11/20/2016.
  */
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
+    private Button bLogin;
+    private EditText editTextEmail;
+    private EditText editTextPass;
+    private TextView textViewSignup;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth Auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        bLogin = (Button) findViewById(R.id.buttonLogin);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPass = (EditText) findViewById(R.id.editTextPass);
+        textViewSignup = (TextView) findViewById(R.id.textViewSignUp);
+        progressDialog = new ProgressDialog(this);
+
+        Auth = FirebaseAuth.getInstance();
+        if (Auth.getCurrentUser() != null) {
+            finish();
+            startActivity(new Intent(getApplicationContext(), User.class));
+        }
+
+        bLogin.setOnClickListener(this);
+        textViewSignup.setOnClickListener(this);
+    }
+
+
+    private void userLogin() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPass.getText().toString().trim();
+
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progressDialog.setMessage("Login User...");
+        progressDialog.show();
+        //end validasi form OK
+
+        //proses login, pencocokan data dengan firebase
+        Auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        if (task.isSuccessful()) {
+                            finish();
+                            startActivity(new Intent(getApplicationContext(), User.class));
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Password atau Username salah", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == bLogin) {
+            userLogin();
+        }
+        if (view == textViewSignup) {
+            finish();
+            startActivity(new Intent(this, MainActivity.class));
+        }
+
     }
 }
+
